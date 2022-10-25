8a15e3d6301100fb7d4d41e11752689bcc3ff9bb
Daniel Ramos <danielramos@microsoft.com>
2022-10-25 14:03:08 -0700

    Manually turn on activity IDs

--- RuntimeMonitor.cs
+++ RuntimeMonitor.cs
@@ -1,18 +1,6 @@
 ﻿using System.Diagnostics.Tracing;
+using Microsoft.Extensions.Logging;
 
-public struct RuntimeEventMonitor : IDisposable
-{
-    public static RuntimeEventMonitor StartNew(RuntimeContext context)
-    {
-        RuntimeActivityEventListener.Singleton.t_currentContext.Value = context;
-        return new RuntimeEventMonitor();
-    }
-
-    public void Dispose()
-    {
-        RuntimeActivityEventListener.Singleton.t_currentContext.Value = null;
-    }
-}
 
 class ActivityGeneratingEventSource : EventSource 
 {
@@ -27,15 +15,16 @@ class ActivityGeneratingEventSource : EventSource
     {
         WriteEvent(2);
     }
-    
 }
-
 
 class RuntimeActivityEventListener : EventListener
 {
+    private ILogger m_logger;
     private static object s_consoleLock = new object();
-    static internal RuntimeActivityEventListener Singleton = new RuntimeActivityEventListener();
-    internal AsyncLocal<RuntimeContext?> t_currentContext = new AsyncLocal<RuntimeContext?>();
+    public RuntimeActivityEventListener(ILogger logger)
+    {
+        m_logger = logger;
+    }
 
     public Dictionary<string, Stack<EventWrittenEventArgs>> eventGroupings = new Dictionary<string, Stack<EventWrittenEventArgs>>();
 
@@ -48,17 +37,19 @@ class RuntimeActivityEventListener : EventListener
             // Keyword 0x11 is Runtime events
             EnableEvents(eventSource, EventLevel.Verbose, (EventKeywords)(0x1 | 0x10));
         }
-        // else if (eventSource.Name == "Microsoft-Diagnostics-DiagnosticSource")
-        // {
-        //     Dictionary<string, string> args = new Dictionary<string, string>();
-        //     // DiagnosticSourceEventSource has this domain specific language to tell it what
-        //     // you want to get events for. '[AS]*' tells it to give you all System.Diagnostic.Activity
-        //     // events.
-        //     args["FilterAndPayloadSpecs"] = "[AS]*";
-        //     EnableEvents(eventSource, EventLevel.Verbose, (EventKeywords)0x2, args);
-        // }
+        else if (eventSource.Name == "Microsoft-Diagnostics-DiagnosticSource")
+        {
+            Dictionary<string, string> args = new Dictionary<string, string>();
+            // DiagnosticSourceEventSource has this domain specific language to tell it what
+            // you want to get events for. '[AS]*' tells it to give you all System.Diagnostic.Activity
+            // events.
+            args["FilterAndPayloadSpecs"] = "[AS]*";
+            EnableEvents(eventSource, EventLevel.Verbose, (EventKeywords)0x2, args);
+        }
         else if (eventSource.Name == "System.Threading.Tasks.TplEventSource") 
         {
+            // Activity IDs aren't enabled by default.
+            // Enabling Keyword 0x80 on the TplEventSource turns them on
             EnableEvents(eventSource, EventLevel.Informational, (EventKeywords)0x80);
         }
         else if (eventSource.Name == "ActivityGeneratingEventSource") 
@@ -74,34 +65,36 @@ class RuntimeActivityEventListener : EventListener
         String eventDataActivityID = eventData.ActivityId.ToString();
         lock (s_consoleLock)
         {
-
-            t_currentContext.Value?.LogRuntimeEvent(eventData.EventName, eventDataActivityID);
             if (!eventGroupings.ContainsKey(eventDataActivityID))
             {
                 eventGroupings.Add(eventDataActivityID, new Stack<EventWrittenEventArgs>());
             }
 
-            if (eventData.EventName == "UserDefinedStart")
+            if (eventData.EventName == "ActivityStart")
             {
+                Console.WriteLine($"{eventData.EventName} with ID: {eventDataActivityID}");
                 eventGroupings[eventDataActivityID].Push(eventData);
             }
-            else if (eventData.EventName == "UserDefinedStop")
+            else if (eventData.EventName == "ActivityStop")
             {
+                Console.WriteLine($"{eventData.EventName} with ID: {eventDataActivityID}");
+
                 eventGroupings[eventDataActivityID].Pop();
             }
             else
             {
+                Console.WriteLine($"{eventData.EventName} with ID: {eventDataActivityID}");
                 if (eventGroupings[eventDataActivityID].Count > 0)
                 {
                     EventWrittenEventArgs mappedActivity = eventGroupings[eventDataActivityID].Peek();
                     object[] arguments = (object[])mappedActivity.Payload[2];
                     string activityID = GetIDFromActivityPayload(arguments);
 
-                    //t_currentContext.Value?.LogRuntimeEvent(eventData.EventName, activityID);
+                    //m_logger.LogInformation($"Runtime event {eventData.EventName} is associated with activity {activityID}" );
                 }
                 else
                 {
-                    //t_currentContext.Value?.LogRuntimeEvent(eventData.EventName, null);
+                    //m_logger.LogInformation($"Runtime event {eventData.EventName} has no associated activity.");
                 }
             }
         }
